b2043c62bc8cf250928a558e47a3d72b877302a1
edwarhman <edwarhman@gmail.com>
2022-02-16 23:56:08 -0400

    TASK-24: add comments to Voting contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,7 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+///@title A voting system
+///@author Emerson Warhman
+///@notice You can use this contract to manage a voting
+///
 contract Voting is Ownable {
+  ///@dev voters can have 3 status: unregistered, registered, and has voted
   enum VOTER_STATUS {
     unregistered, registered, hasVoted
   }
@@ -14,52 +19,68 @@ contract Voting is Ownable {
     uint votes;
   }
 
+  ///@notice returns the max number of candidates permited
   uint public maxCandidates;
 
   //colection of data
+  ///@notice returns the specified by his ID
+  ///@param id the candidate's position in the record
   Candidate[] public candidates;
+
+  ///@dev check if address is a candidate
   mapping(address => bool) addressIsCandidate;
+  ///@notice returns the specified voter status 
+  ///@param address The address to check the status
   mapping(address => VOTER_STATUS) public voters;
 
-  //Timer variables
+  ///@dev variables required to restrict voting functions
   uint public countdown;
   uint public startTime;
 
   //events
+
+  ///@notice when a candidate is posted emmit his name, address and id
   event CandidatePosted (
     string indexed name,
     address indexed addr,
     uint indexed id
   );
-
+  ///@notice when a voter is registered or has vote for a candidate emmit the status change 
   event VoterStatusChanged (
     address addr,
     VOTER_STATUS indexed status
   ); 
 
+  ///@notice when a voter vote for a candidate emit the operation 
   event NewVote (
     address voter,
     address indexed candidate,
     uint candidateVotes
   );
 
+  ///@dev set the max candidates permited, the time when the voting starts and the countdown to voting closing
   constructor() {
     maxCandidates = 5;
     startTime = block.timestamp;
     countdown = 1 weeks;
   }
 
+  ///@notice check if the voting is open to do operations 
   modifier votingIsOpen() {
     require(block.timestamp < startTime + countdown, "The voting is close.");
     _;
   }
 
+  ///@notice register a new voter in the voting
   function registerVoter() external votingIsOpen {
     require(voters[msg.sender] == VOTER_STATUS.unregistered, "The user address is already registered");
     voters[msg.sender] = VOTER_STATUS.registered;
     emit VoterStatusChanged(msg.sender, voters[msg.sender]);
   }
 
+  ///@notice post a new candidate to the voting using the specified name and address
+  ///@param _name Candidate name
+  ///@param _address Candidate address
   function postCandidate(string memory _name, address _address) external votingIsOpen onlyOwner {
     require(candidates.length < maxCandidates, "There are the maximum number of candidates.");
     require(addressIsCandidate[_address] == false, "The specified address already is a candidate.");
@@ -68,6 +89,8 @@ contract Voting is Ownable {
     emit CandidatePosted(_name, _address, candidates.length - 1);
   }
 
+  ///@notice sender voter votes for a candidate specified by his ID
+  ///@param candidateId Candidate position in the record
   function voteForCandidate(uint candidateId) external votingIsOpen {
     require(candidateId < candidates.length, "The specified ID is invalid.");
     require(msg.sender != candidates[candidateId].addr, "A candidate is trying to vote for himself");
